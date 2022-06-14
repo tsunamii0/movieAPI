13a430d83ce971caac205532b59cd6099f71576c
cunami <79466733+tsunamii0@users.noreply.github.com>
2022-06-14 18:36:37 +0200

    Add files via upload

---
+++ movieAPI/movieAPI/script.js
@@ -0,0 +1,44 @@
+const mostPopular = document.querySelector('.mostPop')
+const mov0 = document.querySelector('.movieImg0')
+const poster = document.getElementsByTagName('img')
+
+
+const baseURL = 'https://api.themoviedb.org/3/movie/popular?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=1';
+
+
+
+const fetchMov = async () => {
+    const users = await fetch(baseURL)
+    .catch(err => console.log(err))
+    const movies = await users.json().catch(err => console.log(err))
+    console.log(movies)
+    movies.results.forEach(value => {
+        console.log(`${value.poster_path}`)
+        const img = document.createElement('img');
+        img.addEventListener('click', getMovie)
+function getMovie() {
+    const div = document.createElement('div')
+    const h1 = document.createElement('h1');
+    const description = document.createElement('p');
+    const x = document.createElement('p')
+    x.innerHTML = '✕'
+    h1.innerHTML = `${value.title}`
+    description.innerHTML = `${value.overview}`
+    div.appendChild(x)
+    div.appendChild(h1)
+    div.appendChild(description)
+    div.style.backgroundImage = 'url(' + 'https://image.tmdb.org/t/p/w500/' +  value.backdrop_path + ')'
+    div.setAttribute('class', 'movieShow');
+    x.setAttribute('class', 'xBTN')
+    document.body.appendChild(div);
+    x.addEventListener('click', hideMov)
+function hideMov() {
+    div.style.display = ('none')
+}
+}
+    img.src = 'https://image.tmdb.org/t/p/w500/' +  value.poster_path
+    mostPopular.appendChild(img);
+    });
+}
+
+fetchMov()
